613f66fd78d780c456e65e129ecc872ca9b8eee2
id3adl0rd <raindeadobzor2@gmail.com>
2023-12-08 13:52:20 +0300

    validate email method

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	desc "github.com/Task_tracker_back/pkg/user_v1"
+	"github.com/Task_tracker_back/pkg/db"
+	"github.com/Task_tracker_back/pkg/utils"
+	"github.com/Task_tracker_back/user_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
@@ -14,14 +16,14 @@ import (
 const grpcPort = 50051
 
 type server struct {
-	desc.UnimplementedUserV1Server
+	__.UnimplementedUserV1Server
 }
 
 // GETUser ПОЛУЧЕНИЕ ПОЛЬЗОВАТЕЛЯ
-func (s *server) GetUser(ctx context.Context, req *desc.GetRequestUser) (*desc.GetResponseUser, error) {
+func (s *server) GetUser(ctx context.Context, req *__.GetRequestUser) (*__.GetResponseUser, error) {
 	log.Printf("User id: %d", req.GetIdUser())
 
-	return &desc.GetResponseUser{ //TODO: прописать запросы к БД, чтобы возвращали данные из БД.
+	return &__.GetResponseUser{ //TODO: прописать запросы к БД, чтобы возвращали данные из БД.
 		IdUser:    req.GetIdUser(),
 		Login:     "Test Subject",
 		IsManager: false,
@@ -30,28 +32,37 @@ func (s *server) GetUser(ctx context.Context, req *desc.GetRequestUser) (*desc.G
 
 // Валидация логина и пароля пользователя
 // TODO: Добавить запрос к бд, проверяющий пароль и логин
-func (s *server) ValidateUser(ctx context.Context, req *desc.GetRequestAuth) (*desc.GetResponseAuth, error) {
-	if req.GetLogin() == "" {
-		return &desc.GetResponseAuth{IsValidated: false}, errors.New("Login invalid")
+func (s *server) ValidateUser(ctx context.Context, req *__.GetRequestAuth) (*__.GetResponseAuth, error) {
+	login := req.GetLogin()
+	password := req.GetPassword()
+
+	if login == "" {
+		return &__.GetResponseAuth{IsValidated: false}, errors.New("Login invalid")
 	}
 
-	if req.GetPassword() == "" {
-		return &desc.GetResponseAuth{IsValidated: false}, errors.New("Password invalid")
+	if password == "" {
+		return &__.GetResponseAuth{IsValidated: false}, errors.New("Password invalid")
 	}
 
-	return &desc.GetResponseAuth{IsValidated: false}, nil
+	return &__.GetResponseAuth{IsValidated: false}, nil
 }
 
 // Изменение email пользователя
 // TODO: Запрос к БД (конкретный id, email).
-func (s *server) UpdateMail(ctx context.Context, req *desc.PutRequestMail) (*desc.PutResponseMail, error) {
+func (s *server) UpdateMail(ctx context.Context, req *__.PutRequestMail) (*__.PutResponseMail, error) {
 	idUser := req.GetIdUser()
+	mail := req.GetEmail()
 	fmt.Println("Email: ", req.GetEmail(), ", UserID: ", idUser)
-	if req.GetEmail() == "" {
-		return &desc.PutResponseMail{}, errors.New("Mail invalid or missing!")
+
+	if mail == "" {
+		return &__.PutResponseMail{}, errors.New("Mail invalid or missing!")
 	}
 
-	return &desc.PutResponseMail{Email: req.GetEmail()}, nil
+	if utils.ValidateEmail(mail) {
+		return &__.PutResponseMail{Email: mail}, nil
+	}
+
+	return &__.PutResponseMail{}, errors.New("Mail invalid or missing!")
 }
 
 func main() {
@@ -62,9 +73,10 @@ func main() {
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterUserV1Server(s, &server{})
+	__.RegisterUserV1Server(s, &server{})
 
 	log.Printf("server listening at %d", lis.Addr())
+	db.DatabaseConnection()
 
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %d", err)
---
+++ pkg/utils/utils.go
@@ -0,0 +1,10 @@
+package utils
+
+import (
+	"regexp"
+)
+
+func ValidateEmail(mail string) bool {
+	regex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	return regex.MatchString(mail)
+}
